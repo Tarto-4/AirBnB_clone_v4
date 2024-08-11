4aba545aa7090fc4cbd440474f24b503a775eeb4
Thato Mongwe <thatomongwe47@gmail.com>
2024-08-11 17:52:41 +0000

    sys files

--- models/__init__.py
+++ models/__init__.py
@@ -10,23 +10,3 @@ def hbnb():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-"""
-initialize the models package
-"""
-
-from models.engine.db_storage import DBStorage
-from models.engine.file_storage import FileStorage 
-from os import getenv
-
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-from flask import Flask, render_template
-import uuid
-    storage = FileStorage()
-storage.reload()
