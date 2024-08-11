3135e13f422672d21597d82e5d2470a1a48b85f1
Thato Mongwe <thatomongwe47@gmail.com>
2024-08-11 21:37:37 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,6 +4,9 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
